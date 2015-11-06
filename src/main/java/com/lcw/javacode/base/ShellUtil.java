473e744a3608f60e8aa06a01695918e6d2fbae89
Lcw <lcw2004@163.com>
2015-11-06 15:59:21 +0800

    Shell脚本工具类

---
+++ src/main/java/com/lcw/javacode/base/ShellUtil.java
@@ -0,0 +1,49 @@
+package com.lcw.javacode.base;
+
+
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
+public class ShellUtil {
+
+    /**
+     * 在Linux下执行命令
+     * @param cmd
+     * @return
+     */
+    public static String execShellInLinux(String cmd) {
+        Process process = null;
+        try {
+            String[] cmdA = { "/bin/sh", "-c", cmd };
+            process = Runtime.getRuntime().exec(cmdA);
+            LineNumberReader br = new LineNumberReader(new InputStreamReader(process.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            process.destroy();
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(process != null) {
+                process.destroy();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 在Windows下执行命令
+     * @param cmd
+     * @return
+     */
+    public static void execInWindows(String cmd) {
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
