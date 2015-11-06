fd92f19e497524034f58f9cad132fa41562b1959
Lcw <lcw2004@163.com>
2015-11-06 16:18:33 +0800

    完善测试基础类

--- src/test/java/com/lcw/javacode/BaseTestCase.java
+++ src/test/java/com/lcw/javacode/BaseTestCase.java
@@ -1,5 +1,6 @@
 package com.lcw.javacode;
 
+import com.lcw.javacode.config.Log4jLoader;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +13,7 @@ public class BaseTestCase {
 
     @Before
     public void before() {
+        Log4jLoader.loadLog4j();
         start = System.currentTimeMillis();
     }
 
@@ -20,5 +22,30 @@ public class BaseTestCase {
         end = System.currentTimeMillis();
 
         logger.info(String.format("耗时：%d ms", Long.valueOf(end - start)));
+    }
+
+}
+
+/**
+ * 用于打印方法执行的探针
+ */
+class MethodPin {
+    private static Long timeBefore;
+    private static Long timeAfter;
+    private static int time = 0;
+
+    public static void insert() {
+        if(timeBefore == null) {
+            timeBefore = System.currentTimeMillis();
+        }
+        if(timeAfter == null) {
+            timeAfter = System.currentTimeMillis();
+        }
+        timeBefore = timeAfter;
+        timeAfter = System.currentTimeMillis();
+        ++time;
+        System.out.println("timeBefore:" + timeBefore);
+        System.out.println("timeAfter:" + timeAfter);
+        System.out.println("耗时" + time + ":" + (timeAfter - timeBefore));
     }
 }
